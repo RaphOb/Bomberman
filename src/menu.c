a6909357aac3c0e414060dace38b29b302741ac6
Geoffrey <geoffrey.bukin@hotmail.fr>
2019-02-18 22:44:57 +0100

    Modif menu + input menu for port and ip

--- src/menu.c
+++ src/menu.c
@@ -2,4 +2,35 @@
 // Created by oraph on 18/02/2019.
 //
 
+#include <SDL_quit.h>
+#include <SDL_events.h>
+#include <stdio.h>
 #include "../header/menu.h"
+
+
+int menuEvent()
+{
+    int res = 0;
+    SDL_Event event;
+
+    if (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            res = -1;
+        } else if (event.type == SDL_KEYDOWN) {
+            switch (event.key.keysym.sym) {
+                case SDLK_p:
+                    res = 1;
+                    break;
+                case SDLK_q:
+                case SDLK_ESCAPE :
+                    res = -1;
+                    break;
+                default :
+                    fprintf(stderr,"touche inconnue %d\n", event.key.keysym.sym);
+                    break;
+            }
+        }
+    }
+
+    return res;
+}

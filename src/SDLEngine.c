931f2a17ba74d233e95b2a0cf9b9ae8c3f10b2c0
Raph <obadia_r@etna-alternance.net>
2019-03-02 14:00:55 +0100

    coomit song

--- src/SDLEngine.c
+++ src/SDLEngine.c
@@ -77,6 +77,64 @@ void playSound(char* path)
     }
 }
 
+int playsound(char* path)
+{
+
+    // Initialize SDL.
+    if (SDL_Init(SDL_INIT_AUDIO) < 0)
+        return 1;
+
+    // local variables
+    static Uint32 wav_length; // length of our sample
+    static Uint8 *wav_buffer; // buffer containing our audio file
+    static SDL_AudioSpec wav_spec; // the specs of our piece of music
+
+
+    /* Load the WAV */
+    // the specs, length and buffer of our wav are filled
+    if( SDL_LoadWAV(path, &wav_spec, &wav_buffer, &wav_length) == NULL ){
+        return 1;
+    }
+    // set the callback function
+    wav_spec.callback = my_audio_callback;
+    wav_spec.userdata = NULL;
+    // set our global static variables
+    audio_pos = wav_buffer; // copy sound buffer
+    audio_len = wav_length; // copy file length
+
+    /* Open the audio device */
+    if ( SDL_OpenAudio(&wav_spec, NULL) < 0 ){
+        fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
+        exit(-1);
+    }
+
+    /* Start playing */
+    SDL_PauseAudio(0);
+
+    // wait until we're don't playing
+    if ( audio_len == 0 ) {
+        SDL_Delay(100);
+    SDL_CloseAudio();
+    SDL_FreeWAV(wav_buffer);
+    }
+
+    // shut everything down
+
+}
+
+void my_audio_callback(void *userdata, Uint8 *stream, int len) {
+
+    if (audio_len ==0)
+        return;
+
+    len = ((Uint32)len > audio_len ? audio_len :(Uint32)len );
+    SDL_memcpy (stream, audio_pos, len); 					// simply copy from one buffer into the other
+    SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);// mix from one buffer into another
+
+    audio_pos += len;
+    audio_len -= len;
+}
+
 /**
  * function: destroy SDL
  * @param pSDL
@@ -372,6 +430,6 @@ void initMap(sdl_t *pSDL)
         }
     }
     SDL_FreeSurface(map);
-    map = NULL;
+//    map = NULL;
 }
-//la un commentaire on s'en fou
+

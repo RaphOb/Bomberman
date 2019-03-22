c85055bc3a8e27efb73589f293c01350ad08bce6
Raph <obadia_r@etna-alternance.net>
2019-03-22 11:08:58 +0100

    range bomb player

--- src/SDLEngine.c
+++ src/SDLEngine.c
@@ -142,7 +142,7 @@ void my_audio_callback(void *userdata, Uint8 *stream, int len) {
     if (audio_len != 0) {
         len = ((Uint32) len > audio_len ? audio_len : (Uint32) len);
         SDL_memcpy(stream, audio_pos, (Uint32) len);
-        SDL_MixAudio(stream, audio_pos, (Uint32) len, SDL_MIX_MAXVOLUME / 3);
+        SDL_MixAudio(stream, audio_pos, (Uint32) len, SDL_MIX_MAXVOLUME / 4);
 
         audio_pos += len;
         audio_len -= len;
